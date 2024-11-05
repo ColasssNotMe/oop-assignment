63c3d6cdaefe3aaab284e4533626499494988a2f
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2024-11-05 15:03:28 +0800

    Add title panel to MainMenu with styled label

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,9 +1,21 @@
 package view;
 
+import java.awt.Label;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Panel;
+
 public class MainMenu extends BaseFrame {
     public MainMenu() {
         super();
         setTitle("Main Menu");
         setVisible(true);
+        Label titleText = new Label("Main Menu");
+        Panel titlePanel = new Panel(new FlowLayout(FlowLayout.CENTER, 0, 10));
+        Font titleFont = new Font("Arial", Font.PLAIN, 36);
+        titleText.setFont(titleFont);
+        titlePanel.add(titleText);
+        add(titlePanel);
     }
+
 }
