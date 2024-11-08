542d9dc1bf458c404b0065fcff09892cef29d6f1
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2024-11-08 11:08:00 +0800

    added json lib

--- src/util/Crud.java
+++ src/util/Crud.java
@@ -1,8 +1,11 @@
 package util;
 
-// 
+import org.json.JSONObject;
+
 public class Crud {
     public void retrieveJson() {
+        String jsonString = "{\"name\":\"John\", \"age\":30}";
+        JSONObject jsonObject = new JSONObject(jsonString);
 
     }
 
